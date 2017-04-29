99222b1642a1de66e22924802460839967b6cdf4
Max Karel <maxdkarel@gmail.com>
2017-04-29 16:30:08 +0200

    Add timing and output folder
      Remove algorithm for solving system of linear functions
      Implement CUDA algorithm with random potential on both sides of eq
      Remove useless arrays
      Tidy up output folders and files
      Add timers

--- anderson/anderson.cu
+++ anderson/anderson.cu
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 #include <fstream>
+#include <cstdio>
+#include <ctime>
+
 #include <thrust/complex.h>
 #include <thrust/copy.h>
 #include <cublas_v2.h>
 #include "helper_cuda.h"
 
+#define LATTICE_SIZE 512
+
 using namespace std;
 
 typedef thrust::complex<double> th_complex;
 
-const int xmax = 256;
-const int tmax = 500;
+const int xmax = LATTICE_SIZE;
+const int tmax = 50;
 double dt = 0.02;
 //float dx = 2*sqrt(dt);
 double scale = 2.0;
@@ -21,76 +27,33 @@ th_complex imag_one (0.0, 1.0);
 th_complex a = -imag_one*th_complex(dt/(dx*dx),0.0);
 th_complex b = 1.0+a;
 
-__global__ void cutrid_RC_1b(th_complex *a,th_complex *b,th_complex *c,th_complex *d,th_complex *x) {
-  int idx_global=blockIdx.x*blockDim.x+threadIdx.x;
-  int idx=threadIdx.x;
-
-  __shared__ th_complex asub[255];
-  __shared__ th_complex bsub[255];
-  __shared__ th_complex csub[255];
-  __shared__ th_complex dsub[255];
-
-  asub[idx]=a[idx_global];
-  bsub[idx]=b[idx_global];
-  csub[idx]=c[idx_global];
-  dsub[idx]=d[idx_global];
-  __syncthreads();
-  //Reduction
-  for(int stride=1;stride<255;stride*=2) {
-    int margin_left=(idx-stride);
-    int margin_right=(idx+stride);
-    if(margin_left<0) margin_left=0;
-    if(margin_right>=255) margin_right=254;
-    th_complex tmp1 = asub[idx] / bsub[margin_left];
-    th_complex tmp2 = csub[idx] / bsub[margin_right];
-    th_complex tmp3 = dsub[margin_right];
-    th_complex tmp4 = dsub[margin_left];
-    __syncthreads();
-
-    dsub[idx] = dsub[idx] - tmp4*tmp1-tmp3*tmp2;
-    bsub[idx] = bsub[idx]-csub[margin_left]*tmp1-asub[margin_right]*tmp2;
-
-    tmp3 = -csub[margin_right];
-    tmp4 = -asub[margin_left];
-
-    __syncthreads();
-    asub[idx] = tmp3*tmp1;
-    csub[idx] = tmp4*tmp2;
-    __syncthreads();
-  }
-
-  x[idx_global]=dsub[idx]/bsub[idx];
-
-}
-
-__global__ void altCU(th_complex* d_u, th_complex* d_uH, th_complex* d_V,
+__global__ void altCUc(th_complex* d_u, th_complex* d_uH, th_complex* d_V,
             int xmax, th_complex a, th_complex b) {
 
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if ((i<xmax-1)&(i>0)) {
-    th_complex mod_rs[201];  //modified right side
-    th_complex c[201];  //super-diagonal vector
+    th_complex mod_rs[LATTICE_SIZE];  //modified right side
+    th_complex d_c[LATTICE_SIZE];
     //calculate h_c
     for(int j = 0 ; j < xmax ; j++) {
-      c[j] = -a/2.0;	//spodna diagonala v matici, je pri \psi(t-\Delta)
+      d_c[j] = -a/2.0;	//spodna diagonala v matici, je pri \psi(t-\Delta)
     }
     //modify h_c
-    c[0] /= b - d_V[i*xmax + 0];	//delime strednou diagonalou
+    d_c[0] /= b - 0.5*d_V[i*xmax+0];	//delime strednou diagonalou
     for(int j = 1 ; j < xmax ; j++) {
-      c[j] /= (b - d_V[i*xmax + j]) + a/2.0*c[j-1];	//spodna diagonala v matici je -a/2 preto +
+      d_c[j] /= (b - 0.5*d_V[i*xmax+j]) + a/2.0*d_c[j-1];	//spodna diagonala v matici je -a/2 preto +
     }
 
-    mod_rs[0]  = (1.0-a)*d_uH[i*xmax + 0] + a/2.0*(d_uH[(i-1)*xmax + 0]+d_uH[(i+1)*xmax + 0]);
-    mod_rs[0] /= b - d_V[i*xmax + 0];
+    mod_rs[0]  =  (0.5*d_V[i*xmax+0] + 1.0-a)*d_uH[i*xmax+0] + a/2.0*(d_uH[(i-1)*xmax+0]+d_uH[(i+1)*xmax+0]);
+    mod_rs[0] /= b - 0.5*d_V[i*xmax+0];
     th_complex di;  //unmodified right side, help variable
     for(int j=1; j < xmax-1; j++) {
-      di  = (1.0-a)*d_uH[i*xmax + j] + a/2.0*(d_uH[(i-1)*xmax + j]+d_uH[(i+1)*xmax + j]);
-      mod_rs[j] = (di+a/2.0*mod_rs[j-1])/((b - d_V[i*xmax + j])+a/2.0*c[j-1]);
+      di = (0.5*d_V[i*xmax+j] + 1.0-a)*d_uH[i*xmax+j] + a/2.0*(d_uH[(i-1)*xmax+j]+d_uH[(i+1)*xmax+j]);
+      mod_rs[j] = (di+a/2.0*mod_rs[j-1])/((b - 0.5*d_V[i*xmax+j])+a/2.0*d_c[j-1]);
     }
-    d_u[i*xmax + xmax-1]=0; //mod_rs[j];
+    d_u[i*xmax+xmax-1]=0; //mod_rs[j];
     for(int j=xmax-2; j>0; j--) {
-      d_u[i*xmax + j]=mod_rs[j]-c[j]*d_u[i*xmax + j+1];
-
+      d_u[i*xmax+j]=mod_rs[j]-d_c[j]*d_u[i*xmax+j+1];
     }
   }
 }
@@ -103,8 +66,7 @@ __global__ void transposeCU(th_complex* d_u,
     d_uH[i*xmax+j] = d_u[j*xmax+i];
   }
 }
-void printInitialVariables(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_complex h_V[][xmax], th_complex h_c[]);
-void printResult(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_complex h_V[][xmax], th_complex h_c[]);
+void printResult(th_complex h_u[][xmax],ofstream& data_file, ofstream& time_file, float elapsed_time, string platform);
 void initializeHostArrays(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_complex h_V[][xmax], th_complex h_c[]);
 void stdDev_r(ofstream& r, float t, th_complex u[][xmax]);
 void transpose(th_complex arr[][xmax]);
@@ -120,29 +82,57 @@ th_complex h_u[xmax][xmax] = {}; //alocating on heap
 th_complex h_uH[xmax][xmax] = {};
 th_complex h_V[xmax][xmax] = {};
 th_complex h_c[xmax] = {};
-th_complex h_b[xmax] = {};
-th_complex h_a[xmax] = {};
-th_complex h_d[xmax] = {};
-th_complex h_x[xmax] = {};
 
 int main() {
 
   if(xmax > 1024) {printf("Size of arr is greater than maximal number of threads %i\n", xmax);}
 
-
   float arrSize = sizeof(th_complex) * xmax * xmax;
   float vektSize = sizeof(th_complex) * xmax;
+  //Create folder and output files
+  string DIR = "results_anderson/";
+  string DATA_GPU = "data_gpu.dat";
+  string DATA_CPU = "data_cpu.dat";
+  string DEV_GPU = "dev_gpu.dat";
+  string DEV_CPU = "dev_cpu.dat";
+  string DATA_TIME = "time.dat";
+  const char* MKDIR = "mkdir -p ";
 
-  initializeHostArrays(h_u, h_uH, h_V, h_c);
+  string size_of_lattice = to_string(xmax);
+  string scale_of_lattice = to_string((int)scale);
+  string number_of_iterations = to_string(tmax);
+  DIR += "x_"+size_of_lattice+"_s_"+scale_of_lattice+"_t_"+number_of_iterations+"/";
+  const char* PATH = DIR.c_str();
+
+  int LEN_MKDIR = strlen(MKDIR) + strlen(PATH);
+  char* CMD_MKDIR = (char*)malloc(LEN_MKDIR*sizeof(char));
+  sprintf(CMD_MKDIR, "%s%s", MKDIR, PATH);
+  cout << "Saving output in:  " << PATH << endl;
+  system(CMD_MKDIR);
+
+  DATA_GPU = DIR + DATA_GPU;
+  ofstream out_data_gpu(DATA_GPU);
+  DATA_CPU = DIR + DATA_CPU;
+  ofstream out_data_cpu(DATA_CPU);
+  DEV_GPU = DIR + DEV_GPU;
+  ofstream out_dev_gpu(DEV_GPU);
+  DEV_CPU = DIR + DEV_CPU;
+  ofstream out_dev_cpu(DEV_CPU);
+  DATA_TIME = DIR + DATA_TIME;
+  ofstream out_time(DATA_TIME);
 
   //////////////////////////////////////////////
   ////  CUDA                                ////
   //////////////////////////////////////////////
-  /*
-  ofstream r_foutCU("and_stdDev_rCU.dat");
+
+  cudaEvent_t start_gpu, end_gpu;
+  cudaEventCreate(&start_gpu);
+  cudaEventCreate(&end_gpu);
+  cudaEventRecord(start_gpu);
+
+  initializeHostArrays(h_u, h_uH, h_V, h_c);
 
   th_complex *d_u, *d_uH, *d_V, *d_c;
-  th_complex *d_b, *d_d, *d_a, *d_x;
   cuDoubleComplex alpha = make_cuDoubleComplex(1.0,0.0);
   cuDoubleComplex beta = make_cuDoubleComplex(0.0,0.0);
   const cuDoubleComplex* _alpha = &alpha;
@@ -154,10 +144,6 @@ int main() {
   cudaMalloc(&d_uH, arrSize);
   cudaMalloc(&d_V, arrSize);
   cudaMalloc(&d_c, vektSize);
-  cudaMalloc(&d_a, vektSize);
-  cudaMalloc(&d_b, vektSize);
-  cudaMalloc(&d_d, vektSize);
-  cudaMalloc(&d_x, vektSize);
   checkCudaErrors(cudaMemcpy(d_u, h_u, arrSize, cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(d_uH, h_uH, arrSize, cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(d_V, h_V, arrSize, cudaMemcpyHostToDevice));
@@ -171,27 +157,10 @@ int main() {
 
   for(int i = 0; i < xmax; i++) {
     h_c[i] = -a/2.0;
-    h_a[i] = -a/2.0;
   }
-  cudaMemcpy(d_a, h_a, vektSize, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_c, h_c, vektSize, cudaMemcpyHostToDevice);
+
   for (int t = 0; t< tmax; t++) {
-    cudaMemcpy(h_uH, d_u, arrSize, cudaMemcpyDeviceToHost);
-    for(int i = 1 ; i < xmax ; i++) {
-      for(int j = 0 ; j < xmax ; j++) {
-        h_b[j] = b - h_V[i][j];
-        h_d[j] = (1.0-a)*h_uH[i][0] + a/2.0*(h_uH[i-1][0]+h_uH[i+1][0]);
-      }
-      cudaMemcpy(d_b, h_b, vektSize, cudaMemcpyHostToDevice);
-      cudaMemcpy(d_d, h_d, vektSize, cudaMemcpyHostToDevice);
-      cutrid_RC_1b<<<xmax,xmax>>>(d_a, d_b, d_c, d_d, d_x);
-      cudaMemcpy(h_x, d_x, vektSize, cudaMemcpyDeviceToHost);
-      for(int j = 0 ; j < xmax ; j++) {
-        h_u[i][j] = h_x[j];
-      }
-    }
-    cudaMemcpy(d_u, h_u, arrSize, cudaMemcpyHostToDevice);
-    transpose(h_V);
+    altCUc<<<1,xmax>>>(d_u,d_uH,d_V,xmax,a,b);
     cublasZgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, xmax, xmax,
                 _alpha, d_Vc, xmax,
                 _beta, d_Vc, xmax,
@@ -202,46 +171,64 @@ int main() {
                 _beta, d_uc, xmax,
                 d_uHc, xmax);
 
-
-    // cublasZgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, xmax, xmax,
-    //             _alpha, d_Vc, xmax,
-    //             _beta, d_Vc, xmax,
-    //             d_uHc, xmax);
-    // cudaMemcpy(d_V, d_uH, arrSize, cudaMemcpyDeviceToDevice);
-    // cublasZgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, xmax, xmax,
-    //             _alpha, d_uc, xmax,
-    //             _beta, d_uc, xmax,
-    //             d_uHc, xmax);
+    altCUc<<<1,xmax>>>(d_u,d_uH,d_V,xmax,a,b);
+    cublasZgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, xmax, xmax,
+                _alpha, d_Vc, xmax,
+                _beta, d_Vc, xmax,
+                d_uHc, xmax);
+    cudaMemcpy(d_V, d_uH, arrSize, cudaMemcpyDeviceToDevice);
+    cublasZgeam(handle, CUBLAS_OP_T, CUBLAS_OP_N, xmax, xmax,
+                _alpha, d_uc, xmax,
+                _beta, d_uc, xmax,
+                d_uHc, xmax);
 
     if (t%100==0) {
       cudaMemcpy(h_u, d_u, arrSize, cudaMemcpyDeviceToHost);
-      stdDev_r(r_foutCU,t,h_u);
+      stdDev_r(out_dev_gpu,t,h_u);
     }
   }
 
   cudaMemcpy(h_u, d_u, arrSize, cudaMemcpyDeviceToHost);
-  printResult(h_u, h_uH, h_V, h_c);
   cudaFree(d_u);
   cudaFree(d_V);
   cudaFree(d_uH);
   cudaFree(d_c);
-*/
+
+  cudaEventRecord(end_gpu);
+  cudaEventSynchronize(end_gpu);
+  float time_gpu = 0; //miliseconds
+  cudaEventElapsedTime(&time_gpu, start_gpu, end_gpu);
+  time_gpu /= 1000;
+  cout << "TIME GPU:   " << time_gpu << " s" << endl;
+  printResult(h_u, out_data_gpu, out_time, time_gpu, "GPU");
+
   //////////////////////////////////////////////
   ////  CPU                                 ////
   //////////////////////////////////////////////
-  ofstream r_foutCPU("and_stdDev_rCPU.dat");
-  for (int t = 0; t< tmax; t++) {
-    altCPUa(h_u, h_V, h_c, xmax, a, b);
-    transpose(h_u);
-    transpose(h_V);
-    altCPUa(h_u, h_V, h_c, xmax, a, b);
-    transpose(h_u);
-    transpose(h_V);
-    if (t%100==0) {
-      stdDev_r(r_foutCPU,t,h_u);
+
+  clock_t start_cpu = clock();
+  for(int i = 0 ; i < xmax ; i++) {
+    for(int j = 0 ; j < xmax ; j++) {
+      h_u[i][j] = 0;
     }
   }
-  printResult(h_u, h_uH, h_V, h_c);
+  h_u[xmax/2][xmax/2] = 1.0;
+  for (int t = 0; t< tmax; t++) {
+    altCPUc(h_u, h_V, h_c, xmax, a, b);
+    transpose(h_u);
+    transpose(h_V);
+    altCPUc(h_u, h_V, h_c, xmax, a, b);
+    transpose(h_u);
+    transpose(h_V);
+    if (t%10==0) {
+      stdDev_r(out_dev_cpu,t,h_u);
+    }
+  }
+
+  clock_t end_cpu = clock();
+  double time_cpu = (double)(end_cpu - start_cpu) / CLOCKS_PER_SEC;
+  cout << "TIME CPU:   " << time_cpu << " s" << endl;
+  printResult(h_u, out_data_cpu, out_time, time_cpu, "CPU");
 
   return 0;
 }
@@ -253,32 +240,33 @@ void initializeHostArrays(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_co
       h_uH[i][j]=th_complex(0.0, 0.0);
       h_V[i][j] =2*scale*(double)(rand()%10000/10000.0-0.5);
       h_V[i][j] *= dt/imag_one;
-      //h_V[i][j] = 0;
     }
   }
   //Nastavenie pociatocnych podmienok
   h_u[xmax/2][xmax/2] = th_complex(1.0, 0);
   h_uH[xmax/2][xmax/2] = th_complex(1.0, 0);
 }
-void printInitialVariables(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_complex h_V[][xmax], th_complex h_c[]) {
+void printResult(th_complex h_u[][xmax], ofstream& data_file, ofstream& time_file, float elapsed_time, string platform) {
   cout << "     dx== " << dx <<endl<< "     dt== " << dt << endl << "      a== " << a << endl;
   cout << "      b== " << b << endl;
-}
-void printResult(th_complex h_u[][xmax], th_complex h_uH[][xmax], th_complex h_V[][xmax],th_complex h_c[]) {
-  printInitialVariables(h_u, h_uH, h_V, h_c);
-  ofstream fout("data.dat");
+
   double sum = 0;
   for(int i = 0; i<xmax; i++){
     for (int j = 0; j < xmax; j++) {
       double probability = h_u[i][j].real()*h_u[i][j].real();
       probability += h_u[i][j].imag()*h_u[i][j].imag();
-      fout << i << " " << j << " " <<  probability << " " << endl;
+      data_file << i << " " << j << " " <<  probability << " " << endl;
       sum += probability;
     }
   }
-  fout.close();
   cout << h_u[xmax/2][xmax/2] << endl;
   cout << "sum == " << sum << endl;
+
+  time_file << "***************" << endl;
+  time_file << "Runtime for " << platform << " was: " << elapsed_time << " s" << endl;
+  time_file << "Probability sum was: " << sum << endl;
+  time_file << "Probability in middle (" << xmax << "," << xmax << ") was" << h_u[xmax/2][xmax/2] << endl;
+
 }
 void stdDev_r(ofstream& r, float t, th_complex u[][xmax]) {
   th_complex sum = 0;
@@ -373,11 +361,11 @@ void altCPUb(th_complex h_u[][xmax], th_complex h_V[][xmax], th_complex h_c[],
 
   for(int i = 1; i<xmax-1; i++) {
 
-    mod_rs[0]  = h_V[i][0] + (1.0-a)*h_uH[i][0] + a/2.0*(h_uH[i-1][0]+h_uH[i+1][0]);
+    mod_rs[0]  =  (1.0-a+h_V[i][0])*h_uH[i][0] + a/2.0*(h_uH[i-1][0]+h_uH[i+1][0]);
     mod_rs[0] /= b;
     th_complex di;  //unmodified right side, help variable
     for(int j=1; j < xmax-1; j++) {
-      di = h_V[i][j] + (1.0-a)*h_uH[i][j] + a/2.0*(h_uH[i-1][j]+h_uH[i+1][j]);
+      di = (1.0-a+h_V[i][0])*h_uH[i][j] + a/2.0*(h_uH[i-1][j]+h_uH[i+1][j]);
       mod_rs[j] = (di+a/2.0*mod_rs[j-1])/((b)+a/2.0*h_c[j-1]);
     }
     h_u[i][xmax-1]=0; //mod_rs[j];
@@ -424,11 +412,11 @@ void altCPUc(th_complex h_u[][xmax], th_complex h_V[][xmax], th_complex h_c[],
       h_c[j] /= (b - 0.5*h_V[i][j]) + a/2.0*h_c[j-1];	//spodna diagonala v matici je -a/2 preto +
     }
 
-    mod_rs[0]  = 0.5*h_V[i][0] + (1.0-a)*h_uH[i][0] + a/2.0*(h_uH[i-1][0]+h_uH[i+1][0]);
+    mod_rs[0]  =  (0.5*h_V[i][0] + 1.0-a)*h_uH[i][0] + a/2.0*(h_uH[i-1][0]+h_uH[i+1][0]);
     mod_rs[0] /= b - 0.5*h_V[i][0];
     th_complex di;  //unmodified right side, help variable
     for(int j=1; j < xmax-1; j++) {
-      di = 0.5*h_V[i][j] + (1.0-a)*h_uH[i][j] + a/2.0*(h_uH[i-1][j]+h_uH[i+1][j]);
+      di = (0.5*h_V[i][j] + 1.0-a)*h_uH[i][j] + a/2.0*(h_uH[i-1][j]+h_uH[i+1][j]);
       mod_rs[j] = (di+a/2.0*mod_rs[j-1])/((b - 0.5*h_V[i][j])+a/2.0*h_c[j-1]);
     }
     h_u[i][xmax-1]=0; //mod_rs[j];
